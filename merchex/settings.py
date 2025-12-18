af734b529a579f3f21a0e6d4492791637246d642
kevinBissouth <kevinbissouth237@gmail.com>
2025-12-18 21:54:53 +0100

    feat: update4 setting deployment

--- merchex/settings.py
+++ merchex/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-#%x2b8o-efb$i^-m4x1@fsc25+@n#*yeusawy0q+f^qhd-m-(@'
+SECRET_KEY = 'django-insecure-#%x2b8o-efb$i^-m4x1@fsc25+@n#*yeusawy0q+f^qhd-m-(@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
