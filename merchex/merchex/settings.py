65538094b9a8208461ff4e2dfdb8e0209b9b03a6
kevinBissouth <kevinbissouth237@gmail.com>
2025-12-18 21:07:24 +0100

    feat: update settings deployment

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -168,6 +168,15 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "listings/static"),  # chemin vers votre dossier static
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # dossier pour collectstatic
 
 
 
